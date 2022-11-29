9d196ea1a19af88ffe682e1b3409d60b35ad3250
xxw” <x152371628213@163.com>
2022-11-29 17:05:53 +0800

    添加lo4j日志整合demo

--- src/main/java/com/itself/mall/SpringbootDemoApplication.java
+++ src/main/java/com/itself/mall/SpringbootDemoApplication.java
@@ -10,9 +10,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  */
 @EnableScheduling
 @SpringBootApplication
-public class SelfMallApplication {
+public class SpringbootDemoApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SelfMallApplication.class, args);
+        SpringApplication.run(SpringbootDemoApplication.class, args);
         System.out.println("(♥◠‿◠)ﾉﾞ    启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
                 " .-------.       ____     __        \n" +
                 " |  _ _   \\      \\   \\   /  /    \n" +
