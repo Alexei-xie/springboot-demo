84f9d3cc1f050aaadfedb1d6d8fa56c18fd2227f
xxw” <x152371628213@163.com>
2023-01-11 14:28:13 +0800

    启动类输出标记代码

--- src/main/java/com/itself/mall/SpringbootDemoApplication.java
+++ src/main/java/com/itself/mall/SpringbootDemoApplication.java
@@ -13,15 +13,15 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class SpringbootDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDemoApplication.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ    启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
-                " .-------.       ____     __        \n" +
-                " |  _ _   \\      \\   \\   /  /    \n" +
-                " | ( ' )  |       \\  _. /  '       \n" +
-                " |(_ o _) /        _( )_ .'         \n" +
-                " | (_,_).' __  ___(_ o _)'          \n" +
-                " |  |\\ \\  |  ||   |(_,_)'         \n" +
-                " |  | \\ `'   /|   `-'  /           \n" +
-                " |  |  \\    /  \\      /           \n" +
-                " ''-'   `'-'    `-..-'              ");
+        System.out.println("#*#*#*#*#*#*#*#*#*\n" +
+                            "      #*        #*\n" +
+                            "      #*        #*\n" +
+                            "      #*        #*\n" +
+                            "      #*        #*\n" +
+                            "      #*    #*  #*\n" +
+                            "      #*     #* #*\n" +
+                            "      #*          \n" +
+                            "      #*          \n" +
+                            "#*#*#*#*#*#*#*#*#*");
     }
 }
