7be2e2e6856a0e9d881f9eb73240dbd0d5f8ee36
xxw <x15237168213@163.com>
2023-04-22 20:41:12 +0800

    添加xxl-job注释说明

--- src/main/java/com/itself/mall/demo/XxlJobTest.java
+++ src/main/java/com/itself/mall/demo/XxlJobTest.java
@@ -6,6 +6,10 @@ import com.xxl.job.core.handler.annotation.XxlJob;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+/**
+ * 需要结合xxl-job服务进行使用
+ * 先启动XxlJobAdminApplication服务作为xxl-job管理平台
+ */
 @Slf4j
 @Component
 public class XxlJobTest {
